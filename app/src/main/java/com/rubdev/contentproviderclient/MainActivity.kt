4556ed373e1675c2f08b020441d000e5a9ccb622
Rubens <rubao.ma@gmail.com>
2021-04-25 14:41:23 -0300

    first commit

---
+++ app/src/main/java/com/rubdev/contentproviderclient/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.rubdev.contentproviderclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRecycler: RecyclerView
+    lateinit var noteRefreshButton: FloatingActionButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        noteRefreshButton = findViewById(R.id.client_button_refresh)
+        notesRecycler = findViewById(R.id.client_list)
+
+        noteRefreshButton.setOnClickListener {
+
+        }
+
+    }
+}
