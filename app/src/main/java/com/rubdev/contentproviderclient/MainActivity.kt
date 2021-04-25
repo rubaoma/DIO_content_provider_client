753738f6bbba13f50bce72650b6aa2884731c213
Rubens <rubao.ma@gmail.com>
2021-04-25 14:57:45 -0300

    implementation is complete

--- app/src/main/java/com/rubdev/contentproviderclient/MainActivity.kt
+++ app/src/main/java/com/rubdev/contentproviderclient/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.rubdev.contentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -10,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var notesRecycler: RecyclerView
     lateinit var noteRefreshButton: FloatingActionButton
 
+//    val adapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         noteRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
         noteRefreshButton.setOnClickListener {
-
+            getContentProvider()
         }
 
+    }
+
+    private fun getContentProvider(){
+        try {
+            val url = "content://com.rubdev.contentproviderdemo.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? = contentResolver.query(data, null, null, null,"title")
+
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        }catch (ex: Exception){
+            ex.printStackTrace()
+        }
     }
 }
